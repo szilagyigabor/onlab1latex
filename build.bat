588888f5fdb24077abdab70cc6c69173e85d4c8e
Gábor Szilágyi <gbr.szilagyi@gmail.com>
2022-04-11 15:52:26 +0200

    not enough work

---
+++ build.bat
@@ -0,0 +1,8 @@
+set doc=onlab1
+pdflatex -interaction=nonstopmode -halt-on-error %doc%.tex
+bibtex %doc%
+pdflatex -interaction=nonstopmode -halt-on-error %doc%.tex
+pdflatex -interaction=nonstopmode -halt-on-error %doc%.tex
+
+@del %doc%.aux %doc%.bbl %doc%.blg %doc%.log %doc%.out %doc%.toc
+@del tartalom\*.aux
--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 pdflatex -interaction=nonstopmode -halt-on-error onlab1.tex
-bibtex -terse onlab1
+echo
+echo Press enter to continue...
+echo
+read var
+bibtex onlab1
 pdflatex -interaction=nonstopmode -halt-on-error onlab1.tex
 pdflatex -interaction=nonstopmode -halt-on-error onlab1.tex
 
 rm onlab1.aux onlab1.bbl onlab1.blg onlab1.log onlab1.out onlab1.toc
 rm tartalom/*.aux
+
