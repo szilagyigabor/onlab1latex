fa4294dded245d2b4e281fcb857b4c6042414a00
Gábor Szilágyi <gbr.szilagyi@gmail.com>
2022-04-12 15:54:30 +0200

    build scripts prompt for enter at the end

--- build.bat
+++ build.bat
@@ -6,3 +6,5 @@ pdflatex -interaction=nonstopmode -halt-on-error %doc%.tex
 
 @del %doc%.aux %doc%.bbl %doc%.blg %doc%.log %doc%.out %doc%.toc
 @del tartalom\*.aux
+@echo off
+set /P id="press enter to continue..."
--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 pdflatex -interaction=nonstopmode -halt-on-error onlab1.tex
-echo
-echo Press enter to continue...
-echo
-read var
+
 bibtex onlab1
 pdflatex -interaction=nonstopmode -halt-on-error onlab1.tex
 pdflatex -interaction=nonstopmode -halt-on-error onlab1.tex
@@ -11,3 +8,5 @@ pdflatex -interaction=nonstopmode -halt-on-error onlab1.tex
 rm onlab1.aux onlab1.bbl onlab1.blg onlab1.log onlab1.out onlab1.toc
 rm tartalom/*.aux
 
+echo Press enter to continue...
+read var
